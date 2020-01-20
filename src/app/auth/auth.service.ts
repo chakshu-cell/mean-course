e1cf0eba5b26cf802a9c872e71aa6b6ed400701b
Chakshu Arora <chakshuarora6996@gmail.com>
2020-01-20 23:16:32 +0530

    upto video 12

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,13 +4,29 @@ import { AuthData } from './auth-data.model';
 
 @Injectable({ providedIn: 'root'})
 export class AuthService {
+
+  private token: string;
   constructor(private http: HttpClient) {}
+
+  getToken() {
+    return this.token;
+  }
 
   createdUser(email: string, password: string) {
     const authData: AuthData = {email, password};
-    this.http.post('http://localhost:3000/api/user/signup', authData)
+    this.http.post<{token: string}>('http://localhost:3000/api/user/signup', authData)
     .subscribe(response => {
-      console.log(response);
+      const token = response.token;
+      this.token = token;
+    });
+  }
+
+  login(email: string, password: string) {
+    const authData: AuthData = {email, password};
+    this.http.post<{token: string}>('http://localhost:3000/api/user/login', authData)
+    .subscribe(response => {
+      const token = response.token;
+      this.token = token;
     });
   }
 }
